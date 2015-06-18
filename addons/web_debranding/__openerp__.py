1a3ad2f8b8ec106c7498d11fa6a6469eb1684708
unknown <deddy_marzuki@yahoo.com.sg>
2015-06-19 00:32:02 +0800

    added debranding odoo module

---
+++ addons/web_debranding/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Backend debranding",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Custom',
+    'website': 'https://yelizariev.github.io',
+    'depends': ['web'],
+    'data': [
+        'views.xml',
+        'js.xml',
+        ],
+    'installable': True
+}
