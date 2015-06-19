a4d2954ef4ac45a2d278252859a29c6abe84ea24
unknown <deddy_marzuki@yahoo.com.sg>
2015-06-19 18:30:54 +0800

    added customer display module from OCA

---
+++ addons/pos_customer_display/pos_customer_display.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    POS Customer Display module for Odoo
+#    Copyright (C) 2014 Aurélien DUMAINE
+#    Copyright (C) 2015 Akretion (www.akretion.com)
+#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class PosConfig(models.Model):
+    _inherit = 'pos.config'
+
+    iface_customer_display = fields.Boolean(
+        string='Customer Display', help="Display data on the customer display")
+    customer_display_line_length = fields.Integer(
+        string='Line Length', default=20,
+        help="Length of the LEDs lines of the customer display")
