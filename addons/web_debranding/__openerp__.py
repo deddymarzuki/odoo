6c6ca9f3e800559bd6e919417d12501538c80392
unknown <deddy_marzuki@yahoo.com.sg>
2015-08-23 10:28:27 +0800

    added debranding cost

---
+++ addons/web_debranding/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Backend debranding",
+    'version': '1.0.0',
+    'author': 'Ivan Yelizariev',
+    'category': 'Debranding',
+    'website': 'https://yelizariev.github.io',
+    'price': 81.00,
+    'currency': 'EUR',
+    'depends': ['web', 'share', 'disable_openerp_online', 'mail_delete_sent_by_footer'],
+    'data': [
+        'security/web_debranding_security.xml',
+        'data.xml',
+        'views.xml',
+        'js.xml',
+        'pre_install.yml',
+        ],
+    'installable': True
+}
